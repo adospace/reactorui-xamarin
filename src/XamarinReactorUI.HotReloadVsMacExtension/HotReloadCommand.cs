6df8841ad550f5cedf440315133815a36a032060
adospace <adospace@msn.com>
2020-05-01 23:49:57 +0100

    Initial Vs MAc Extension

---
+++ src/XamarinReactorUI.HotReloadVsMacExtension/HotReloadCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Mono.Addins;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using MonoDevelop.Projects;
+
+namespace XamarinReactorUI.HotReloadVsMacExtension
+{
+    public class HotReloadCommandHandler : CommandHandler
+    {
+        protected override void Run()
+        {
+            TryGetReactorUIProject(out var projectFile);
+            base.Run();
+        }
+
+        private bool TryGetReactorUIProject(out string projectFile)
+        {
+            projectFile = null;
+
+            var solution = IdeApp.Workspace.CurrentSelectedSolution;
+            if (solution != null)
+            {
+                foreach (var project in solution.GetAllProjects().OfType<DotNetProject>())
+                {
+                    Console.WriteLine(project.Name);
+                }
+            }
+
+            return false;
+        }
+
+        protected override void Update(CommandInfo info)
+        {
+            base.Update(info);
+        }
+    }
+}

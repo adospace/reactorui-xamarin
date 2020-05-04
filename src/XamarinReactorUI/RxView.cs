0383926f6526da357e7fe010475ea8255c017842
Ado Marinucci <ado@realtimeresearch.com>
2020-05-05 00:36:12 +0200

    Completing gestures

--- src/XamarinReactorUI/RxView.cs
+++ src/XamarinReactorUI/RxView.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Xamarin.Forms;
 using XamarinReactorUI.Internals;
 
@@ -12,14 +10,21 @@ namespace XamarinReactorUI
         LayoutOptions VerticalOptions { get; set; }
         Thickness Margin { get; set; }
 
-        Action OnTappedAction { get; set; }
-        Action OnDoubleTappedAction { get; set; }
+        Action TapAction { get; set; }
+        Action DoubleTapAction { get; set; }
+
+        Action<PinchGestureUpdatedEventArgs> PinchAction { get; set; }
+        Action<PanUpdatedEventArgs> PanAction { get; set; }
+        Action<SwipedEventArgs> SwipeAction { get; set; }
     }
 
     public abstract class RxView<T> : RxVisualElement<T>, IRxView where T : Xamarin.Forms.View, new()
     {
         private TapGestureRecognizer _tapGestureRecognizer;
         private TapGestureRecognizer _doubleTapGestureRecognizer;
+        private PinchGestureRecognizer _pinchGestureRecognizer;
+        private PanGestureRecognizer _panGestureRecognizer;
+        private SwipeGestureRecognizer _swipeGestureRecognizer;
 
         protected RxView()
         {
@@ -27,18 +32,20 @@ namespace XamarinReactorUI
 
         protected RxView(Action<T> componentRefAction)
             : base(componentRefAction)
-        { 
-        
+        {
         }
 
         public LayoutOptions HorizontalOptions { get; set; } = (LayoutOptions)View.HorizontalOptionsProperty.DefaultValue;
-        
+
         public LayoutOptions VerticalOptions { get; set; } = (LayoutOptions)View.VerticalOptionsProperty.DefaultValue;
-        
+
         public Thickness Margin { get; set; } = (Thickness)View.MarginProperty.DefaultValue;
 
-        public Action OnTappedAction { get; set; }
-        public Action OnDoubleTappedAction { get; set; }
+        public Action TapAction { get; set; }
+        public Action DoubleTapAction { get; set; }
+        public Action<PinchGestureUpdatedEventArgs> PinchAction { get; set; }
+        public Action<PanUpdatedEventArgs> PanAction { get; set; }
+        public Action<SwipedEventArgs> SwipeAction { get; set; }
 
         protected override void OnUpdate()
         {
@@ -46,44 +53,91 @@ namespace XamarinReactorUI
             NativeControl.HorizontalOptions = HorizontalOptions;
             NativeControl.Margin = Margin;
 
-            NativeControl.GestureRecognizers.Clear();
-
-            if (OnTappedAction != null)
-            {
-                NativeControl.GestureRecognizers.Add(_tapGestureRecognizer = new TapGestureRecognizer()
-                {
-                    Command = new ActionCommand(OnTappedAction)
-                });
-            }
-
-            if (OnDoubleTappedAction != null)
-            {
-                NativeControl.GestureRecognizers.Add(_doubleTapGestureRecognizer = new TapGestureRecognizer()
-                {
-                    Command = new ActionCommand(OnDoubleTappedAction),
-                    NumberOfTapsRequired = 2
-                });
-            }
+            AttachEvents();
 
             base.OnUpdate();
         }
 
+        private void AttachEvents()
+        {
+            if (TapAction != null)
+            {
+                NativeControl.GestureRecognizers.Add(_tapGestureRecognizer = new TapGestureRecognizer()
+                {
+                    Command = new ActionCommand(TapAction)
+                });
+            }
+
+            if (DoubleTapAction != null)
+            {
+                NativeControl.GestureRecognizers.Add(_doubleTapGestureRecognizer = new TapGestureRecognizer()
+                {
+                    Command = new ActionCommand(DoubleTapAction),
+                    NumberOfTapsRequired = 2
+                });
+            }
+
+            if (PinchAction != null)
+            {
+                NativeControl.GestureRecognizers.Add(_pinchGestureRecognizer = new PinchGestureRecognizer());
+                _pinchGestureRecognizer.PinchUpdated += PinchGestureRecognizer_PinchUpdated;
+            }
+
+            if (PanAction != null)
+            {
+                NativeControl.GestureRecognizers.Add(_panGestureRecognizer = new PanGestureRecognizer());
+                _panGestureRecognizer.PanUpdated += PanGestureRecognizer_PanUpdated;
+            }
+
+            if (SwipeAction != null)
+            {
+                NativeControl.GestureRecognizers.Add(_swipeGestureRecognizer = new SwipeGestureRecognizer());
+                _swipeGestureRecognizer.Swiped += SwipeGestureRecognizer_Swiped;
+            }
+        }
+
+        private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            PanAction?.Invoke(e);
+        }
+
+        private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+        {
+            SwipeAction?.Invoke(e);
+        }
+
+        private void PinchGestureRecognizer_PinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            PinchAction?.Invoke(e);
+        }
+
+        private void DetachEvents()
+        {
+            if (NativeControl != null)
+            {
+                if (_tapGestureRecognizer != null)
+                    NativeControl.GestureRecognizers.Remove(_tapGestureRecognizer);
+                if (_doubleTapGestureRecognizer != null)
+                    NativeControl.GestureRecognizers.Remove(_doubleTapGestureRecognizer);
+                if (_pinchGestureRecognizer != null)
+                    NativeControl.GestureRecognizers.Remove(_pinchGestureRecognizer);
+                if (_panGestureRecognizer != null)
+                    NativeControl.GestureRecognizers.Remove(_panGestureRecognizer);
+                if (_swipeGestureRecognizer != null)
+                    NativeControl.GestureRecognizers.Remove(_swipeGestureRecognizer);
+            }
+        }
+
         protected override void OnMigrated(VisualNode newNode)
         {
-            if (NativeControl != null && _tapGestureRecognizer != null)
-                NativeControl.GestureRecognizers.Remove(_tapGestureRecognizer);
-            if (NativeControl != null && _doubleTapGestureRecognizer != null)
-                NativeControl.GestureRecognizers.Remove(_doubleTapGestureRecognizer);
+            DetachEvents();
 
             base.OnMigrated(newNode);
         }
 
         protected override void OnUnmount()
         {
-            if (NativeControl != null && _tapGestureRecognizer != null)
-                NativeControl.GestureRecognizers.Remove(_tapGestureRecognizer);
-            if (NativeControl != null && _doubleTapGestureRecognizer != null)
-                NativeControl.GestureRecognizers.Remove(_doubleTapGestureRecognizer);
+            DetachEvents();
 
             base.OnUnmount();
         }
@@ -91,15 +145,33 @@ namespace XamarinReactorUI
 
     public static class RxViewExtensions
     {
-        public static T OnTapped<T>(this T view, Action action) where T : IRxView
+        public static T OnTap<T>(this T view, Action action) where T : IRxView
         {
-            view.OnTappedAction = action;
+            view.TapAction = action;
             return view;
         }
 
-        public static T OnDoubleTapped<T>(this T view, Action action) where T : IRxView
+        public static T OnDoubleTap<T>(this T view, Action action) where T : IRxView
         {
-            view.OnDoubleTappedAction = action;
+            view.DoubleTapAction = action;
+            return view;
+        }
+
+        public static T OnPinch<T>(this T view, Action<PinchGestureUpdatedEventArgs> action) where T : IRxView
+        {
+            view.PinchAction = action;
+            return view;
+        }
+
+        public static T OnPan<T>(this T view, Action<PanUpdatedEventArgs> action) where T : IRxView
+        {
+            view.PanAction = action;
+            return view;
+        }
+
+        public static T OnSwipe<T>(this T view, Action<SwipedEventArgs> action) where T : IRxView
+        {
+            view.SwipeAction = action;
             return view;
         }
 
@@ -156,21 +228,25 @@ namespace XamarinReactorUI
             view.HorizontalOptions = LayoutOptions.Fill;
             return view;
         }
+
         public static T HStartAndExpand<T>(this T view) where T : IRxView
         {
             view.HorizontalOptions = LayoutOptions.StartAndExpand;
             return view;
         }
+
         public static T HCenterAndExpand<T>(this T view) where T : IRxView
         {
             view.HorizontalOptions = LayoutOptions.CenterAndExpand;
             return view;
         }
+
         public static T HEndAndExpand<T>(this T view) where T : IRxView
         {
             view.HorizontalOptions = LayoutOptions.EndAndExpand;
             return view;
         }
+
         public static T HFillAndExpand<T>(this T view) where T : IRxView
         {
             view.HorizontalOptions = LayoutOptions.FillAndExpand;
@@ -206,26 +282,29 @@ namespace XamarinReactorUI
             view.VerticalOptions = LayoutOptions.Fill;
             return view;
         }
+
         public static T VStartAndExpand<T>(this T view) where T : IRxView
         {
             view.VerticalOptions = LayoutOptions.StartAndExpand;
             return view;
         }
+
         public static T VCenterAndExpand<T>(this T view) where T : IRxView
         {
             view.VerticalOptions = LayoutOptions.CenterAndExpand;
             return view;
         }
+
         public static T VEndAndExpand<T>(this T view) where T : IRxView
         {
             view.VerticalOptions = LayoutOptions.EndAndExpand;
             return view;
         }
+
         public static T VFillAndExpand<T>(this T view) where T : IRxView
         {
             view.VerticalOptions = LayoutOptions.FillAndExpand;
             return view;
         }
-
     }
 }

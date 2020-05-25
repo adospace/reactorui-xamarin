85f70f6f770792010605df1a54005aff26ac5a48
adospace <adospace@msn.com>
2020-05-25 19:49:01 +0200

    Animation sample reworking

--- src/XamarinReactorUI.TestApp/XamarinReactorUI.TestApp/Animation/MainPage.cs
+++ src/XamarinReactorUI.TestApp/XamarinReactorUI.TestApp/Animation/MainPage.cs
@@ -14,19 +14,17 @@ namespace XamarinReactorUI.TestApp.Animation
         {
             return new RxContentPage()
             {
-                new RxImage()
-                    .HCenter()
-                    .VCenter()
-                    .Source("city.jpg")
-                    .OnTap(()=>SetState(s => s.Toggle = !s.Toggle))
-                    //.Scale(State.Toggle ? 1.0 : 0.5)
-                    //.Opacity(State.Toggle ? 1.0 : 0.2)
-                    //.WithAnimation(duration: 2000)
-                    .Rotation(State.Toggle ? 180.0 : 0.0)
-                    .WithAnimation(duration: 10000)
-                    //.When(State.Toggle, _=>_.WithAnimation(easing: Easing.BounceIn, duration: 10000))
-                    //.When(!State.Toggle, _=>_.WithAnimation(duration: 1000))
-
+                new RxFrame()
+                { 
+                    new RxImage()
+                        .HCenter()
+                        .VCenter()
+                        .Source("city.jpg")
+                }
+                .OnTap(()=>SetState(s => s.Toggle = !s.Toggle))
+                .Scale(State.Toggle ? 1.0 : 0.5)
+                .Opacity(State.Toggle ? 1.0 : 0.2)
+                .WithAnimation(duration: 1000)
             };
         }
     }

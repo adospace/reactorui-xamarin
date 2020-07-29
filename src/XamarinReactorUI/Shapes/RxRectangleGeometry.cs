07d56e8ef5f5485913fdafa5bc89833672afa565
adospace <adospace@msn.com>
2020-07-29 16:14:24 +0200

    Added RxLineGeometry, RxPathGeometry, RxRectangleGeometry

---
+++ src/XamarinReactorUI/Shapes/RxRectangleGeometry.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
+
+namespace XamarinReactorUI.Shapes
+{
+    public interface IRxRectangleGeometry : IVisualNode
+    {
+        Xamarin.Forms.Rectangle Rect { get; set; }
+    }
+
+    public class RxRectangleGeometry<T> : RxGeometry<T>, IRxRectangleGeometry where T : RectangleGeometry, new()
+    {
+        public RxRectangleGeometry()
+        {
+        }
+
+        public RxRectangleGeometry(Action<T> componentRefAction)
+            : base(componentRefAction)
+        {
+        }
+
+        public Xamarin.Forms.Rectangle Rect { get; set; } = (Xamarin.Forms.Rectangle)RectangleGeometry.RectProperty.DefaultValue;
+
+        protected override void OnUpdate()
+        {
+            NativeControl.Rect = Rect;
+
+            base.OnUpdate();
+        }
+
+        protected override void OnAnimate()
+        {
+            NativeControl.Rect = Rect;
+
+            base.OnAnimate();
+        }
+
+        protected override IEnumerable<VisualNode> RenderChildren()
+        {
+            yield break;
+        }
+    }
+
+    public class RxRectangleGeometry : RxRectangleGeometry<RectangleGeometry>
+    {
+        public RxRectangleGeometry()
+        {
+        }
+
+        public RxRectangleGeometry(Action<RectangleGeometry> componentRefAction)
+            : base(componentRefAction)
+        {
+        }
+    }
+
+    public static class RxRectangleGeometryExtensions
+    {
+        public static T Rect<T>(this T rectanglegeometry, Xamarin.Forms.Rectangle rect) where T : IRxRectangleGeometry
+        {
+            rectanglegeometry.Rect = rect;
+            return rectanglegeometry;
+        }
+    }
+}

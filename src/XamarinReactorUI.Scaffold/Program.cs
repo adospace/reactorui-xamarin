1cb90dfdee62f3b6b942f79b2040d6001b3daf66
Adolfo Marinucci <adospace@msn.com>
2021-03-27 16:38:00 +0100

    Scaffold app widget list

--- src/XamarinReactorUI.Scaffold/Program.cs
+++ src/XamarinReactorUI.Scaffold/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Xml.Linq;
+using Xamarin.Forms;
 
 namespace XamarinReactorUI.Scaffold
 {
@@ -9,12 +11,26 @@ namespace XamarinReactorUI.Scaffold
     {
         public static void Main()
         {
-            var typeToScaffold = typeof(Xamarin.Forms.Shapes.Rectangle);
+            var _ = new Xamarin.Forms.Shapes.Rectangle();
 
-            var outputPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "gen");
-            Directory.CreateDirectory(outputPath);
+            var types = (from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
+                             // alternative: from domainAssembly in domainAssembly.GetExportedTypes()
+                         from assemblyType in domainAssembly.GetTypes()
+                         where typeof(BindableObject).IsAssignableFrom(assemblyType)
+                         // alternative: where assemblyType.IsSubclassOf(typeof(B))
+                         // alternative: && ! assemblyType.IsAbstract
+                         select assemblyType)
+                .ToDictionary(_ => _.FullName, _ => _);
 
-            Scaffold(typeToScaffold, outputPath);
+            foreach (var classNameToGenerate in File.ReadAllLines("WidgetList.txt").Where(_ => !string.IsNullOrWhiteSpace(_)))
+            {
+                var typeToScaffold = types[classNameToGenerate];
+
+                var outputPath = @"..\XamarinReactorUI";
+                Directory.CreateDirectory(outputPath);
+
+                Scaffold(typeToScaffold, outputPath);
+            }
         }
 
         private static void Scaffold(Type typeToScaffold, string outputPath)
